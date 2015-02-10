4d3cd746f56706232bc67076323d10c1c72e7988
swhiteley91 <simonwhiteley@hotmail.com>
2015-02-10 11:04:12 +0100

    first commit

---
+++ src/les2/Customer.java
@@ -0,0 +1,29 @@
+package les2;
+
+import java.util.Date;
+
+public class Customer {
+	
+	private String name;
+	private Date dateOfBirth;
+	private Address address;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+}
